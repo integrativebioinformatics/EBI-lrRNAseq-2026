c66f0f253ca8b1ea59b4c4f6a4c05abdcf442771
Bárbara Borges <66753479+borgessbarbara@users.noreply.github.com>
2026-01-30 13:11:02 -0300

    Add script to download mouse brain sample FASTQ files
    This script downloads multiple mouse brain sample FASTQ files from the EBI FTP server and saves them with appropriate names.

---
+++ scripts-VM/download-mice-samples.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+cd /home/training/course_dir/data_dir/Barbara_Borges/mice_samples/
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR247/018/SRR24781618/SRR24781618_1.fastq.gz -o brain_6m_rep_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR247/017/SRR24781617/SRR24781617_1.fastq.gz -o brain_6m_rep_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR247/016/SRR24781616/SRR24781616_1.fastq.gz -o brain_6m_rep_3.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR247/015/SRR24781615/SRR24781615_1.fastq.gz -o brain_6m_rep_4.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR247/014/SRR24781614/SRR24781614_1.fastq.gz -o brain_24m_rep_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR247/013/SRR24781613/SRR24781613_1.fastq.gz -o brain_24m_rep_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR247/012/SRR24781612/SRR24781612_1.fastq.gz -o brain_24m_rep_3.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR247/011/SRR24781611/SRR24781611_1.fastq.gz -o brain_24m_rep_4.fastq.gz
